99a5b9f481c93b9c70e934b0f875addc830a17b4
Mathieu Henrissat <studio.creatic@gmail.com>
2022-12-20 17:20:50 +0100

    conflit eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import { gql, useQuery } from "@apollo/client";
 import './App.css';
 
+interface  User {
+  id: number;
+  firstname:  string;
+  lastname:  string;
+  email:  string;
+}
+
+const GET_USERS = gql`
+query ListUsers {
+  users {
+    id
+    email
+    firstname
+    lastname
+}
+}
+`
 function App() {
+
+  const { loading, error, data } = useQuery(GET_USERS);
+  console.log(data)
+
+  if (loading) return <div>Chargement en cours</div>;
+  if (error) return <div>Une erreur s'est produite</div>;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Bienvenue sur WinteRent
+        </h1>
+        <div>
+          <ul>
+            {data.users.map((users: User) => (
+              <li key={users.id}>
+                {users.firstname} | {users.lastname}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <form>
+          <h4>Sign up</h4>
+          <div>
+            <input type="text" className="input" placeholder="Name" />
+            <input type="email" className="input" placeholder="Email" />
+            <input type="password" className="input" placeholder="Password" />
+          </div>
+          <button className="submit-btn">S'inscrire</button>
+        </form>
+        <div>Log in</div>
       </header>
     </div>
   );
