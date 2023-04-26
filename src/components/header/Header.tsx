67e840a48b3d49344886fc55bcdc2888067583ba
Mathieu Henrissat <studio.creatic@gmail.com>
2023-04-26 17:23:12 +0200

    amélioration du hover sur la nav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,11 @@
 import { useLogin } from '../../context/LoginProvider';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './_header.scss';
 import { useState } from 'react';
 
 
 const Header = () => {
+  let location = useLocation()
   const { userLog } = useLogin();
   const handleDeleteLocalStorage = () => {
     localStorage.removeItem('userLog');
@@ -18,39 +19,18 @@ const Header = () => {
 
   return userLog ? (
     <>
-      {/* <div  className="nav-elements">
-        <ul>
-          <li>
-            <Link className="active " to="/" aria-current="page">Home</Link>
-          </li>
-          <li>
-            <Link className="" to="/category">Categories</Link>
-          </li>
-          <li>
-            <Link className="" to="/category">Stations</Link>
-          </li>
-          <li>
-            <Link className="" to="/category">A propos</Link>
-          </li>
-          <li>
-            <Link className="" to="/login" onClick={handleDeleteLocalStorage}>
-              deconnexion
-            </Link>
-          </li>
-          <Link className="" to="/login">{userLog.user.firstname}</Link>
-        </ul>
-      </div> */}
       <div className="nav-container">
+        <div className="m-3">Winterent</div>
         <nav className="navbar navbar-expand-lg navbar-dark ">
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse " id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                        </li>
+                    <ul className="nav navbar-nav me-auto mb-2 mb-lg-0">
+                      <li className="nav-item">
+                          <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
+                      </li>
                       <li className="nav-item dropdown">
                           <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" type="button" >Categories</a>
                           <div className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -62,7 +42,7 @@ const Header = () => {
                           </div>
                       </li>
                       <li className="nav-item">
-                          <Link className="nav-link" to="category">Stations</Link>
+                          <Link className={`nav-link ${location.pathname === "/destination" ? "active" : ""}`} to="/destination">Stations</Link>
                       </li>
                       <li className="nav-item">
                         <Link className="nav-link" to="/category">A propos</Link>
@@ -74,8 +54,8 @@ const Header = () => {
                         </Link>
                       </li >
                       <li className="vr opacity-30">|</li>
-                      <li className="nav-item text-center p-2 ">
-                        <Link className="d-flex flex-column text-decoration-none " to="/login"><div><i className="bi bi-person-fill fs-2 "></i></div><div className="user-profil">{userLog.user.firstname}</div></Link>
+                      <li className="nav-item text-center p-2">
+                        <Link className="d-flex flex-column text-decoration-none text-light" to="/login"><div><i className="bi bi-person-fill fs-2 "></i></div><div className="user-profil">{userLog.user.firstname}</div></Link>
                       </li>
                   </ul>
               </div>
