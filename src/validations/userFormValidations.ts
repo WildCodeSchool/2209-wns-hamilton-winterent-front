2b65887bf5e1be77300c579c2d71bc557fcb86d3
Vince-lgd <lagardevincent64@gmail.com>
2023-04-26 11:55:41 +0200

    save

--- src/validations/userFormValidations.ts
+++ src/validations/userFormValidations.ts
@@ -1,21 +1,43 @@
 import * as yup from "yup";
 
-const registerSchema = yup.object().shape({
-  firstName: yup.string().required("First name is required"),
-  lastName: yup.string().required("Last name is required"),
-  email: yup.string().email("Invalid email").required("Email is required"),
-  phoneNumber: yup.string().required("Phone is required"),
-  birthdate: yup.string().required("Birthdate is required"),
+export const registerSchema = yup.object().shape({
+  firstname: yup
+    .string()
+    .required("Votre prénom est obligatoire")
+    .min(3, "Le prénom doit avoir au minimum 3 lettres")
+    .max(30, "Le prénom doit contenir au maximum 30 lettres"),
+  lastname: yup
+    .string()
+    .required("Votre nom est obligatoire")
+    .min(3, "Le prénom doit avoir au minimum 3 lettres")
+    .max(30, "Le prénom doit contenir au maximum 30 lettres"),
+  email: yup
+    .string()
+    .email("L'email fournit n'est pas valide")
+    .required("L'email est obligatoire"),
+  phoneNumber: yup.string().required("Le numéro de téléphone est obligatoire"),
+  birthdate: yup.string().required("La date de naissance est obligatoire"),
   password: yup
     .string()
-    .required("Password is required")
-    .min(6, "Password must be at least 6 characters"),
+    .required("Mot de passe est obligatoire")
+    .min(8, "Le mot de passe doit contenir au minimum 8 caractères")
+    .matches(/[0-9]/, "Le mot de passe doit contenir au minimum 8 caractères")
+    .matches(
+      /[a-z]/,
+      "Le mot de passe doit contenir au minimum 1 lettre en miniscule"
+    )
+    .matches(
+      /[A-Z]/,
+      "Le mot de passe doit contenir au minimum 1 lettre en majuscule"
+    )
+    .matches(
+      /[^\w]/,
+      "Le mot de passe doit contenir au minimum 1 caractère spécial"
+    ),
   confirmPassword: yup
     .string()
-    .oneOf([yup.ref("password")], "Passwords must match")
-    .required("Confirm password is required"),
+    .oneOf([yup.ref("password")], "Il doit être identique au mot de passe")
+    .required("La confirmation du mot de passe est obligatoire"),
 });
 
-export default registerSchema;
-
-const addressSchema = yup.object().shape({});
+export const addressSchema = yup.object().shape({});
