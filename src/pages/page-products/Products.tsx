7ac5bdab0de910cb0fcb007a6ce73d9490f671d6
s.boutier <s.boutier@LGOP4422>
2023-04-28 09:50:08 +0200

    finalisation de la mise en forme du composant SkiCard (statique)

--- src/pages/page-products/Products.tsx
+++ src/pages/page-products/Products.tsx
@@ -1,10 +1,14 @@
-// import SkiCard from "./skiCard/SkiCard";
-
+import SkiCard from "../../components/skiCard/SkiCard";
+import './Products.scss';
 const Products = () => {
     return (
-        <div>
+        <div className="products">
+            <div className="destination-page-image d-flex justify-content-center flex-column align-items-center">
+                <div className="text-white d-flex flex-column justify-content-center"></div>
+            </div>
+            <div>liste input</div>
             <div className="d-flex justify-content-center">
-                <div className="w-50">
+                <div className="w-75">
                     <h3 className="text-primary">
                         <i className="bi bi-chevron-double-down"></i> NOS SKIS
                     </h3>
@@ -14,7 +18,7 @@ const Products = () => {
                         omnis nihil reprehenderit sunt excepturi voluptatum autem, soluta
                         magni cupiditate explicabo quis.
                     </div>
-                    {/* <SkiCard/> */}
+                    <SkiCard/>
                 </div>
             </div>
         </div>
