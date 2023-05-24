9a442c67e59d60478fe5f32edb0b958e23ec6b02
davy lebris <davylebris@gmail.com>
2023-05-24 12:11:15 +0200

    ajout de useSearchParams dans url pour recup infos category et shop

--- src/components/utils/SearchBar.tsx
+++ src/components/utils/SearchBar.tsx
@@ -1,162 +1,148 @@
-import React from 'react'
-import './SearchBar.scss'
-import { useState } from "react"
+import React from 'react';
+import './SearchBar.scss';
+import { useState } from 'react';
 import { SHOPS } from '../../graphql/queries/shopQuery';
 import { useLazyQuery, useQuery } from '@apollo/client';
 import { LIST_CATEGORY } from '../../graphql/queries/categoryQuery';
 import { SEARCHBAR } from '../../graphql/queries/searchbarQuery';
-// import { useShopContext } from "";
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+interface Test {
+  category: string;
+}
 
 interface Shop {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
 interface CategoryName {
-    id: string;
-    category: string;
+  id: string;
+  category: string;
 }
 
-
 export default function SearchBar() {
-    //date du jour
-    let currentDate = new Date().toISOString().slice(0, 10)
+  //date du jour
 
-    //Sélectionner la date sélectionner dans le calendrier avec onChange
-    const [dateArrival, setDateArrival] = useState<string | null>('');
-    const [dateDeparture, setDateDeparture] = useState<string | null>(''); 
-    const [categories, setCategories] = useState<CategoryName[]>([]);
-    const [selectCategories, setSelectCategories] = useState('')
-const [selectShop, setSelectShop] = useState('');
-    
-    // récupérer les shops
-    const [shops, setShops] = useState<Shop[]>([]);
-    const { loading, error } = useQuery(SHOPS, {
-        onCompleted(data) {
-          setShops(data.shops);
-        },
-    });
+  let currentDate = new Date().toISOString().slice(0, 10);
 
-    const [productsFilter, { loading: loadingSearchbar, error: errorSearchbar }] = useLazyQuery(
-      SEARCHBAR,
-      {
-        variables: {
-          idCategory: selectCategories,
-          idShop: selectShop,
-        },
-        onCompleted(data) {
-          console.log(data.productsFilter, 'test avec valeur');
-        },
-      }
-    );
+  //Sélectionner la date sélectionner dans le calendrier avec onChange
+  const [dateArrival, setDateArrival] = useState<string | null>('');
+  const [dateDeparture, setDateDeparture] = useState<string | null>('');
+  const [categories, setCategories] = useState<CategoryName[]>([]);
+  const [selectCategories, setSelectCategories] = useState('');
+  const [selectShop, setSelectShop] = useState('');
 
-    const handleChange = () => {
-        productsFilter({
-          variables: {
-            idCategory: selectCategories,
-            idShop: selectShop,
-          },
-        });
-    } 
+  // récupérer les shops
+  const [shops, setShops] = useState<Shop[]>([]);
+  const navigator = useNavigate();
 
-    //Récupérer les catégories
-    const { loading: loadingCategory, error: errorCategory } = useQuery(LIST_CATEGORY, {
+  const { loading, error } = useQuery(SHOPS, {
+    onCompleted(data) {
+      setShops(data.shops);
+    },
+  });
+  
+  const handleClick = () => {
+    navigator(`/products?category=${selectCategories}&shops=${selectShop}`);
+  };
+
+  //Récupérer les catégories
+  const { loading: loadingCategory, error: errorCategory } = useQuery(
+    LIST_CATEGORY,
+    {
       onCompleted(data) {
         setCategories(data.listCategory);
       },
-    });
+    }
+  );
 
-    const onChangeArrival = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const newArrival = e.target.value
-      setDateArrival(newArrival)
-  
-    }
-    const onChangeDeparture = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const newDeparture = e.target.value
-      setDateDeparture(newDeparture)
-  
-    }
+  const onChangeArrival = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newArrival = e.target.value;
+    setDateArrival(newArrival);
+  };
+  const onChangeDeparture = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newDeparture = e.target.value;
+    setDateDeparture(newDeparture);
+  };
 
-    //condition la date de départ plus récent que date d'arriver
-    if (dateArrival && dateDeparture){
-        if (dateArrival > dateDeparture)
-        {
-            alert("Votre date d'arrivée ne peux pas être plus récente que votre date de départ");
-            //dateDeparture.innerHTML = "";
-        }
+  //condition la date de départ plus récent que date d'arriver
+  if (dateArrival && dateDeparture) {
+    if (dateArrival > dateDeparture) {
+      alert(
+        "Votre date d'arrivée ne peux pas être plus récente que votre date de départ"
+      );
+      //dateDeparture.innerHTML = "";
     }
-    if (loading) return <div>Chargement en cours</div>;
-    if (error) return <div>Une erreur s'est produite</div>;
-    return (
-      <form className="search-bar rounded border border-primary">
-        <select
-          className="product-search rounded-left"
-          defaultValue=""
+  }
+  if (loading || loadingCategory) return <div>Chargement en cours</div>;
+  if (error || errorCategory) return <div>Une erreur s'est produite</div>;
+  return (
+    <form className="search-bar rounded border border-primary">
+      <select className="product-search rounded-left" defaultValue="" required>
+        <option value="" disabled selected>
+          Catégories
+        </option>
+        {categories.map((category) => (
+          <option
+            onClick={() => setSelectCategories(category.id)}
+            value={category.category}
+            selected
+            key={category.id}
+          >
+            {category.category}
+          </option>
+        ))}
+      </select>
+      <select className="product-search border-left" defaultValue="" required>
+        <option value="" disabled selected>
+          Magasins
+        </option>
+        {shops.map((shop) => (
+          <option
+            onClick={() => setSelectShop(shop.id)}
+            value={shop.name}
+            selected
+            key={shop.id}
+          >
+            {shop.name}
+          </option>
+        ))}
+      </select>
+      <div className="date-start ">
+        <input
+          className="block-arrival search-date"
+          type="date"
+          placeholder="selectionner arrivée"
+          pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+          title="yyyy-mm-dd"
+          min={currentDate}
+          name="arrival"
+          onChange={onChangeArrival}
           required
-        >
-          <option value="" disabled selected>
-            Catégories
-          </option>
-          {categories.map((category) => (
-            <option
-              onClick={() => setSelectCategories(category.id)}
-              value={category.category}
-              selected
-              key={category.id}
-            >
-              {category.category}
-            </option>
-          ))}
-        </select>
-        <select className="product-search border-left" defaultValue="" required>
-          <option value="" disabled selected>
-            Magasins
-          </option>
-          {shops.map((shop) => (
-            <option
-              onClick={() => setSelectShop(shop.id)}
-              value={shop.name}
-              selected
-              key={shop.id}
-            >
-              {shop.name}
-            </option>
-          ))}
-        </select>
-        <div className="date-start ">
-          <input
-            className="block-arrival search-date"
-            type="date"
-            placeholder="selectionner arrivée"
-            pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
-            title="yyyy-mm-dd"
-            min={currentDate}
-            name="arrival"
-            onChange={onChangeArrival}
-            required
-          />
-        </div>
-        <div className="date-end search-date ">
-          <input
-            className="block-departure search-date"
-            type="date"
-            placeholder="selectionner départ"
-            pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
-            title="yyyy-mm-dd"
-            min={currentDate}
-            name="departure"
-            onChange={onChangeDeparture}
-            required
-          />
-        </div>
-        <button
-        onClick={handleChange}
-          type="button"
-          value="Rechercher"
-          className="btn_travelSearch rounded-right"
-        >
-          Chercher
-        </button>
-      </form>
-    );
+        />
+      </div>
+      <div className="date-end search-date ">
+        <input
+          className="block-departure search-date"
+          type="date"
+          placeholder="selectionner départ"
+          pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+          title="yyyy-mm-dd"
+          min={currentDate}
+          name="departure"
+          onChange={onChangeDeparture}
+          required
+        />
+      </div>
+      <button
+        onClick={handleClick}
+        type="button"
+        value="Rechercher"
+        className="btn_travelSearch rounded-right"
+      >
+        Rechercher
+      </button>
+    </form>
+  );
 }
