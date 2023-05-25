1808382880797d1e646f4ec61108b11617e613f0
s.boutier <s.boutier@LGOP4422>
2023-05-25 17:13:09 +0200

    création de la page panier

---
+++ src/pages/page-cart/Cart.tsx
@@ -0,0 +1,20 @@
+import './Cart.scss';
+
+const Cart = () => {
+    return (
+        <div className="cart">
+            <div className="cart-page-image d-flex justify-content-center flex-column align-items-center">
+                <div className="text-white d-flex flex-column justify-content-center"></div>
+            </div>
+            <div className="d-flex justify-content-center">
+                <div className="w-75">
+                    <h3 className="text-primary">
+                        <i className="bi bi-chevron-double-down"></i> PANIER
+                    </h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;
