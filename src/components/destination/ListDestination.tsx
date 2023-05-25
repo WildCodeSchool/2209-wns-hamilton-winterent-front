f006a2d079121841368a230f0e7c62eef8c63add
Mathieu Henrissat <studio.creatic@gmail.com>
2023-05-25 16:57:37 +0200

    ajout de la condition sur le lien vers allProduct dans destination

--- src/components/destination/ListDestination.tsx
+++ src/components/destination/ListDestination.tsx
@@ -11,7 +11,7 @@ const ListDestination = ({ city }: DestinationShop) => {
   return (
     <div className="">
       <div className="well">
-        <Link className="text-decoration-none" to={`/shop/${city}?idCategory=${idCategory}`}>
+        <Link className="text-decoration-none" to={idCategory?`/shop/${city}?idCategory=${idCategory}`: `/shop/${city}?idCategory=allProduct`}>
           <div className="d-flex ms-1 justify-content-between align-items-center text-white">
             <h3>{city}</h3>
             <div>
